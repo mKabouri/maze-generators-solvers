aeca77c92dc1ce6a214792f39c9abc431c18c222
Mohamed Yassine KABOURI <kabouri.yassine@hotmail.com>
2024-01-31 22:01:01 +0100

    Adding argparse

--- src/run.py
+++ src/run.py
@@ -4,13 +4,29 @@ from generators.dfs_gen import DFSGenerator
 from solvers.a_star import AStarSolver
 from solvers.tremaux import TremauxSolver
 from solvers.routing import RoutingSolver
+import argparse
 import config
 
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser(description="Maze Solver")
+    parser.add_argument("--solver", choices=["astar", "tremaux", "routing"], default="astar", help="Solver algorithm to use (default: A*)")
+    args = parser.parse_args()
+
     maze_width, maze_height = config.WIDTH, config.HEIGHT
     tile_size = config.TILE_SIZE
     screen = pygame.display.set_mode((maze_width, maze_height))
 
     maze = Maze(maze_height, maze_width, tile_size, screen)
     generator = DFSGenerator(maze)
-    generator.display_solve_maze(RoutingSolver, gif=False)
+
+    if args.solver == "astar":
+        solver_class = AStarSolver
+    elif args.solver == "tremaux":
+        solver_class = TremauxSolver
+    elif args.solver == "routing":
+        solver_class = RoutingSolver
+
+    generator.display_solve_maze(solver_class, gif=False)
+
+if __name__ == "__main__":
+    main()
